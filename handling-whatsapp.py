9b41c2a73d0acd0aebce512e1b865e3e7f074a27
Nouf Aljowaysir <na1215@nyu.edu>
2018-07-19 20:21:31 -0400

    Update handling-whatsapp.py

--- handling-whatsapp.py
+++ handling-whatsapp.py
@@ -6,33 +6,21 @@ sys.setdefaultencoding('utf-8')
 
 files = os.listdir("whatsapp-corpus/")
 
-# EXTRACTING ONLY ONE-ON-ONE CONVERSATIONS WITH ME
-
+### CLEANING UP WHATSAPP CONVOS
 for f in files:
     messages = open ("whatsapp-corpus/" + f, "r").readlines()
     senders = []
     msg = ''
     i = 0
 
-    # if len(messages) > 1:
-    #     for m in messages:
-    #         # finding names for file
-    #         title = '\](.*?)\:'
-    #         who = re.findall(title, m)
-    #         for name in who:
-    #             index = who.index(name)
-    #             new = name.lstrip()
-    #             name = new
-    #             who.pop(index)
-    #             who.insert(index,name)
-    #             if name not in senders:
-    #                 senders.append(name)
-
     # concacting starts here
     while i < len(messages)-1:
+        # extract senders using regex
         title = '\](.*?)\:'
         sender = re.findall(title, messages[i])
+        #replace line breaks in messages with ' '
         msg += re.sub('\n', ' ', messages[i])
+        #if sender sends more than one message in a row then concatenate messages together
         while sender == re.findall(title, messages[i+1]) or re.findall(title, messages[i+1]) == []:
             msg += ". " + re.sub('\n', ' ', messages[i+1])
             i += 1
@@ -40,6 +28,7 @@ for f in files:
                 break
         msg += "\n"
         i += 1
+    #rewrite file with newly cleaned messages    
     with open("whatsapp-corpus/" + f,'w') as f:
         f.write(msg)
         print "concatenation done"
