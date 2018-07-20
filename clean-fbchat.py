564b214b7e98d6842d99a61567fb348473f9ecfa
Nouf Aljowaysir <na1215@nyu.edu>
2018-07-19 20:48:27 -0400

    Update clean-fbchat.py

--- clean-fbchat.py
+++ clean-fbchat.py
@@ -4,12 +4,11 @@ import os
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 links = []
 
 files = os.listdir("corpus/")
-#print files
 
+### CLEANING FACEBOOK MESSAGES FROM YOUTUBE LINKS, SYMBOLS, FILE EXTENSIONS ETC 
 for f in files:
     list_sentences = {}
     list_replace = {}
@@ -21,16 +20,14 @@ for f in files:
 
     messages = encoded_messages
 
-    # CLEANING UP LINKS
-    # SAVING MY OWN LINKS IN A SEPARATE FILE
-
+    # extracting links from messages I have sent using regex and saving them in a seperate file
     for message in messages[1:len(messages):2]:
         url = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         find = re.findall(url, message)
         if find:
             links.append(str(find) + '\n')
-    #print links
-
+    
+    # extracting links sent to me using regex and removing them
     for message in messages:
         url = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         find = re.findall(url, message)
@@ -42,8 +39,7 @@ for f in files:
             messages.pop(i)
             messages.insert(i,message)
 
-    # CLEANING UP RANDOM APOSTROPHES, SPACES, ETC
-
+    # cleaning up random apostrophes, spaces, etc
     for message in messages:
         apostrophe = message.replace('&#039;', "'")
         spaces = apostrophe.replace('.  ', "")
@@ -59,8 +55,7 @@ for f in files:
             messages.pop(i)
             messages.insert(i,message)
 
-    # CLEANING UP HTML TAGS
-
+    # cleaning up html tags
     for message in messages:
         tag = """</?\w+((\s+\w+(\s*=\s*(?:".*?"|'.*?'|[\^'">\s]+))?)+\s*|\s*)/?>"""
         find = re.findall(tag, message)
@@ -72,24 +67,18 @@ for f in files:
             messages.pop(i)
             messages.insert(i,message)
 
-    # FINDING QUESTIONS
-
+    # Using NLTK, extracting questions sent to me
     for message in messages[::2]:
         #print message
         list_sentences[messages.index(message)] = nltk.sent_tokenize(message)
-        #list_sentences.append(nltk.sent_tokenize(message))
-
-    #print list_sentences
 
     for sentences in list_sentences.items():
-        #print sentences[1]
         for sentence in sentences[1]:
             words = nltk.word_tokenize(sentence)
             num = words.count('?')
             if num > 1:
                 words.remove('?')
-            #print words
-            #print num
+
             for word in words:
                 if word == '?' and sentences[0] not in list_replace:
                     #print sentence
@@ -97,17 +86,17 @@ for f in files:
                     list_replace[sentences[0]] = sentence + " "
                 elif word == '?' and sentences[0] in list_replace:
                     list_replace[sentences[0]] += sentence + " "
-    #print list_replace
-
+    # replacing those sentences with just the question asked
     for index in list_replace.items():
-        #print index[0]
         messages.pop(index[0])
         messages.insert(index[0],index[1]+ '\n')
     #print messages
     # print "finished cleaning up file: " + str(f)
     # print messages
+    # rewriting file with cleaned version
     with open("corpus/" + f, 'w') as file:
         file.writelines( messages )
 
+# saving links extracted to another file
 with open("links/links.txt", 'w') as file:
     file.writelines( links )
