6d79f3267d24fb7e020d925ea2bc4679acf184cf
Nouf Aljowaysir <na1215@nyu.edu>
2018-07-19 20:54:01 -0400

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -7,44 +7,29 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+### USING NLTK, ANALYZING FREQUENCY OF WORDS IN MY MESSAGES
 frequency = {}
-
 files = os.listdir("corpus/")
 
+# stopwords - don't look for these words for frequency analysis
 stopwords = set(nltk.corpus.stopwords.words('english')) | set((u'hehe', u'hmm', u'lol', u'lols', u'ill', u'haha', u'doesnt', u'ooo', u'anywhoo', u'kinda',u'might', u'didnt', u'also',
 u'cause',u'thats', u'wanna',u'hes',u'well',u'still',u'ohh',u'lemme',u'since',u'lets',u'hows',u'okie',u'havent',u'cant',u'ive',u'dont',u'wanna' ))
 
-#print stopwords
-
 for f in files:
     messages = open ("corpus/" + f, "r").readlines()
-    #print len(messages)
-
+    
+    # analyze messages sent by me 
     for message in messages[1:len(messages):2]:
-        #print message
+        #convert word to lowercase and find words that contain between 3-15 letters 
         lowercase = message.lower()
         words = re.findall(r'\b[a-z]{3,15}\b', lowercase)
-
+        # analyze frequency and add the word and its level of frequency to a dictionary 
         for word in words:
             if word not in stopwords:
-                #print word
                 count = frequency.get(word,0)
                 frequency[word] = count + 1
-
+                
+# print and sort dictionary from lowest to highest frequency value
 for key, value in sorted(frequency.iteritems(), key=lambda (k,v): (v,k)):
     if value > 75:
         print "%s: %s" % (key, value)
-
-# for words in words:
-#     if frequency[words] > 99:
-#         print words, frequency[words]
-
-# nums = frequency.values()
-# words = frequency.keys()
-#
-# m = max(nums)
-#
-# for word in words:
-# #     if frequency[words] > 99:
-#     if frequency[word] == m:
-#         print word, m
